8ba4551aa80ca36e97edc419213c8534380c6fd9
Sarthak Gupta <sarthakgupta4119@gmail.com>
2023-11-01 18:38:16 +0530

    ToDoList

---
+++ src/Mycomponents/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+    // let footerStyle={
+    //     position:"relative" ,
+    //     top:"70vh" ,
+    //     width:"100%",
+
+    // }
+  return (
+    <div className='bg-dark text-light py-3'>
+        <p className="text-center">Copyright &copy; MyToDoList.com</p>
+        
+    </div>
+  )
+}
